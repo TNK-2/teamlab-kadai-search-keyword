23fd9cc40fa9d01f854df4186cdfcc174d07e7ec
Hoang Cuong <hoangcuong@team-lab.com>
2017-10-13 17:13:59 +0900

    投入SQLはdockerイメージに入れる

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
 ADD target/teamlab-spring-boot-docker-0.1.0.jar app.jar
+RUN apk add --no-cache mysql-client
 COPY target/wait.sh wait.sh
+COPY target/mydb.sql mydb.sql
 ENV JAVA_OPTS=""
-ENTRYPOINT /wait.sh
--- wait.sh
+++ wait.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
 set -e
+host="$1"
+shift
+user="$1"
+shift
+password="$1"
+
 
 echo "Waiting for mysql"
-sleep 15
+until mysql -h"$host" -u"$user" -p"$password" mydb < mydb.sql &> /dev/null
+do
+        sleep 1
+        echo "Waiting for mysql"
+done
+
+echo "MySQL is up - executing command"
+
 exec java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app.jar
